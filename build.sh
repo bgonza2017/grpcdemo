4ebb587f898555d0f31ce9683e7469400c17a52d
bgonza2017 <benedicto.gonzales@yum.com>
2017-06-07 11:37:33 -0500

    edit

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -f ./grpcdemo/grpcdemo.pb.go
 rm -f ./server/server
 rm -f ./client/client
 
-#protoc -I grpcdemo/ grpcdemo/grpcdemo.proto --go_out=plugins=grpc:grpcdemo
+protoc -I grpcdemo/ grpcdemo/grpcdemo.proto --go_out=plugins=grpc:grpcdemo
 
 #cd server
 #go build
